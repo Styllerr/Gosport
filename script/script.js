ddb8285090bf09fbddf320d84545d6556467ea17
Yevhenii_Chukavov <cork@ukr.net>
2021-03-22 14:56:16 +0200

    add serction border

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ for (let anchor of anchors) {
     anchor.addEventListener('click', (e) =>{
         e.preventDefault();
         const blockName = anchor.getAttribute('href').slice(1);
-        document.querySelector(`a[name*=${blockName}]`).scrollIntoView({
+        document.querySelector(`a[id*=${blockName}]`).scrollIntoView({
             behavior: 'smooth',
             block: 'start'
         })
