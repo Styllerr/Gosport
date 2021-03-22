e5e41b30ef43a2cf67bfcaa7116b59d72554d0b6
Yevhenii_Chukavov <cork@ukr.net>
2021-03-22 19:57:52 +0200

    add function for hide btn_down

--- script/script.js
+++ script/script.js
@@ -1,11 +1,20 @@
 const anchors = document.querySelectorAll('a[href*="#"]');
 for (let anchor of anchors) {
-    anchor.addEventListener('click', (e) =>{
+    anchor.addEventListener('click', (e) => {
         e.preventDefault();
         const blockName = anchor.getAttribute('href').slice(1);
         document.querySelector(`a[id*=${blockName}]`).scrollIntoView({
             behavior: 'smooth',
             block: 'start'
         })
-    } )
+    })
 }
+
+window.addEventListener('scroll', (e) => {
+    if(window.scrollY > 10) {
+        document.querySelector('.start-screen__btn-down').style.display = 'none';
+        
+    } else {
+        document.querySelector('.start-screen__btn-down').style.display = 'block';
+    }
+})
