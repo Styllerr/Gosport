6e43ad929d40ea8cb335d63c59454dbc51971cbe
Yevhenii_Chukavov <cork@ukr.net>
2021-03-29 12:29:04 +0300

    finish dress page

--- script/index-script.js
+++ script/index-script.js
@@ -27,14 +27,21 @@ window.addEventListener('resize', (e) => {
 })
 
 const mobileMenuBtn = document.querySelector('.mobile-menu__btn');
-
+const darkThema = document.querySelector('.header-dark-thema') || null;
 const topHeader = document.querySelector('.top-header')
 const topMenu = document.querySelector('.top-menu');
 const mobileMenuIcon = document.querySelector('.mobile-menu__icon');
 
 mobileMenuBtn.addEventListener('click', (e) => {
-    topHeader.classList.toggle('header-dark-thema');
+    darkThema ? topHeader.classList.toggle('header-dark-thema')
+    : null
     topMenu.classList.toggle('top-menu_checked');
     mobileMenuIcon.classList.toggle('mobile-menu__icon_checked');
 })
 
+const subMenu = document.querySelector('.sub-menu');
+const subMeluList = document.querySelector('.sub-menu__list');
+subMenu.addEventListener('click', (e) => {
+    subMenu.classList.toggle('sub-menu_show');
+    subMeluList.classList.toggle('sub-menu__list_show');
+})
