edee0f25a5c1dded7d3573ff7a9f3ac3a3976bd0
Yevhenii_Chukavov <cork@ukr.net>
2021-03-30 13:00:13 +0300

    add modol

--- script/index-script.js
+++ script/index-script.js
@@ -1,29 +1,36 @@
-const anchors = document.querySelectorAll('a[href*="#"]');
-for (let anchor of anchors) {
-    anchor.addEventListener('click', (e) => {
-        e.preventDefault();
-        const blockName = anchor.getAttribute('href').slice(1);
-        document.querySelector(`a[id*=${blockName}]`).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        })
+const visualBlock = document.querySelector('.visual-block') || null;
+if(visualBlock) {
+    const anchors = document.querySelectorAll('a[href*="#"]');
+    if(anchors || anchors.length > 0) {
+        for (let anchor of anchors) {
+            anchor.addEventListener('click', (e) => {
+                e.preventDefault();
+                const blockName = anchor.getAttribute('href').slice(1);
+                document.querySelector(`a[id*=${blockName}]`).scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                })
+            })
+        }
+    }
+}
+const startScreenBtn = document.querySelector('.start-screen__btn-down');
+if (startScreenBtn) {
+    window.addEventListener('scroll', (e) => {
+        if (window.scrollY > 10) {
+            startScreenBtn.style.display = 'none';
+
+        } else {
+            startScreenBtn.style.display = 'block';
+        }
     })
 }
-
-window.addEventListener('scroll', (e) => {
-    if(window.scrollY > 10) {
-        document.querySelector('.start-screen__btn-down').style.display = 'none';
-        
-    } else {
-        document.querySelector('.start-screen__btn-down').style.display = 'block';
-    }
-})
 let widthBrouser = window.innerWidth;
 const btnName = document.querySelector('.caption__link > a')
 window.addEventListener('resize', (e) => {
     widthBrouser = window.innerWidth;
     (widthBrouser < 1051) ? btnName.innerText = 'Our catalogue'
-    : btnName.innerHTML = 'Catalogue'
+        : btnName.innerHTML = 'Catalogue'
 })
 
 const mobileMenuBtn = document.querySelector('.mobile-menu__btn');
@@ -34,7 +41,7 @@ const mobileMenuIcon = document.querySelector('.mobile-menu__icon');
 
 mobileMenuBtn.addEventListener('click', (e) => {
     darkThema ? topHeader.classList.toggle('header-dark-thema')
-    : null
+        : null
     topMenu.classList.toggle('top-menu_checked');
     mobileMenuIcon.classList.toggle('mobile-menu__icon_checked');
 })
@@ -44,4 +51,22 @@ const subMeluList = document.querySelector('.sub-menu__list');
 subMenu.addEventListener('click', (e) => {
     subMenu.classList.toggle('sub-menu_show');
     subMeluList.classList.toggle('sub-menu__list_show');
+})
+
+const bronzeLink = document.getElementById('bronzePackageLink');
+const silverLink = document.getElementById('silverPackageLink');
+const goldLink = document.getElementById('goldPackageLink');
+const platinumLink = document.getElementById('platinumPackageLink');
+bronzeLink.addEventListener('click', (e) => {
+    document.querySelector('.packages__modal-conteiner').classList.add('packages__modal-conteiner_active');
+    document.getElementById('bronzePackage').classList.add('modal-card_active');
+})
+silverLink.addEventListener('click', (e) => {
+    document.querySelector('.packages__modal-conteiner').classList.add('packages__modal-conteiner_active');
+})
+goldLink.addEventListener('click', (e) => {
+    document.querySelector('.packages__modal-conteiner').classList.add('packages__modal-conteiner_active');
+})
+platinumLink.addEventListener('click', (e) => {
+    document.querySelector('.packages__modal-conteiner').classList.add('packages__modal-conteiner_active');
 })
